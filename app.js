62cbba4525dea8252cd7eb6c14600f2d3bd2c157
DevUmutYildirim <devumutyildirim@gmail.com>
2022-08-13 02:57:27 +0300

    Template Engine and Middleware

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
 
+
+// MIDDLEWARE
+const myLogger = (req, res, next) => {
+    console.log("Middleware 1");
+    next();
+}
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public'))
+app.use(myLogger);
+
 app.get("/", (req, res) => {
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description" 
-        }
-        res.send(blog);
+    res.render("index");
+})
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+app.get("/add", (req, res) => {
+    res.render("add");
 })
 
 const port = 5000;
